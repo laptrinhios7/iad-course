d93bfbcd21ec1fcfa039964ae40c4224ab7fbd9a
Lap trinh iOS 7 <laptrinhios7@gmail.com>
2014-03-09 10:08:15 +0700

    OK

---
+++ session 2/DataTypesDemo/DataTypesDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DataTypesDemo
+//
+//  Created by Vu Tran Lam on 3/6/14.
+//  Copyright (c) 2014 FPT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
