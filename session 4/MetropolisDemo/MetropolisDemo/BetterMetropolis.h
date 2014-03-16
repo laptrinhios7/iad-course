093c289a342f8b0032dca85566b0c1c0236e1261
Lap trinh iOS 7 <laptrinhios7@gmail.com>
2014-03-16 12:58:18 +0700

    Metropolis Demo

---
+++ session 4/MetropolisDemo/MetropolisDemo/BetterMetropolis.h
@@ -0,0 +1,16 @@
+//
+//  BetterMetropolis.h
+//  MetropolisDemo
+//
+//  Created by Vu Tran Lam on 3/16/14.
+//  Copyright (c) 2014 FPT. All rights reserved.
+//
+
+#import "Metropolis.h"
+#import "City.h"
+
+@interface BetterMetropolis : Metropolis
+
+- (void)displayAllCities;
+
+@end
