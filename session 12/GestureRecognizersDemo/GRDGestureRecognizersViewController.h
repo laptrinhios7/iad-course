eeba686efc2ee396a042acfa63cf5b49c1cd4914
Lap trinh iOS 7 <laptrinhios7@gmail.com>
2014-03-18 09:05:22 +0700

    Gesture Recognizers Demo

---
+++ session 12/GestureRecognizersDemo/GRDGestureRecognizersViewController.h
@@ -0,0 +1,13 @@
+//
+//  GRDGestureRecognizersViewController.h
+//  GestureRecognizersDemo
+//
+//  Created by Vu Tran Lam on 12/12/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRDGestureRecognizersViewController : UIViewController <UIGestureRecognizerDelegate>
+
+@end
