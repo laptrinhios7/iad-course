cb5efeaeb73f30ff80fedde91dda23d606787ed5
Lap trinh iOS 7 <laptrinhios7@gmail.com>
2014-03-18 09:05:37 +0700

    Touches Handling Demo

---
+++ session 12/TouchesHandlingDemo/TouchesHandlingDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchesHandlingDemo
+//
+//  Created by Vu Tran Lam on 3/18/14.
+//  Copyright (c) 2014 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THDAppDelegate class]));
+    }
+}
