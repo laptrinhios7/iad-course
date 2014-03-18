ad1b807c16470b1aa50bda7fdd9236755e76c8d1
Lap trinh iOS 7 <laptrinhios7@gmail.com>
2014-03-18 08:51:18 +0700

    Simple Tab Bar Demo

---
+++ session 13/SimpleTabBarDemo/STBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STBAppDelegate.h
+//  Session11-SimpleTabBarDemo
+//
+//  Created by Vu Tran Lam on 10/14/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ session 13/SimpleTabBarDemo/SimpleTabBarDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTabBarDemo
+//
+//  Created by Vu Tran Lam on 3/18/14.
+//  Copyright (c) 2014 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STBAppDelegate class]));
+    }
+}
